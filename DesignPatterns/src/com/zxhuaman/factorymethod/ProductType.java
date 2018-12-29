aa7d862a6ca994dd1cc3ec84dcfd5e16ec5f9fb6
zengxianghua <zengxianghua@yftech.com>
2018-12-29 14:58:43 +0800

    add:数据存储

--- DesignPatterns/src/com/zxhuaman/factorymethod/ProductType.java
+++
@@ -1,8 +0,0 @@
-package com.zxhuaman.factorymethod;
-
-/**
- * Created by zxhuaman on 2018/6/23.
- */
-public enum  ProductType {
-    PHONE, COMPUTER
-}
--- DesignPatterns/src/com/zxhuaman/prototype/PersonalOrder.java
+++
@@ -1,7 +0,0 @@
-package com.zxhuaman.prototype;
-
-public class PersonalOrder extends Order {
-    public PersonalOrder() {
-        setName("个人订单");
-    }
-}
